e14a33eeee76a4ea4fccad863da3e1b8535aeaa0
Marcio Sansone <msansone@gmail.com>
2025-08-02 15:24:47 +0100

    #3 created transactions service and controller layers

---
+++ src/main/java/br/com/msansone/StockDashboard/service/TransactionServiceImpl.java
@@ -0,0 +1,39 @@
+package br.com.msansone.StockDashboard.service;
+
+import br.com.msansone.StockDashboard.exception.ResourceNotFoundException;
+import br.com.msansone.StockDashboard.model.Transaction;
+import br.com.msansone.StockDashboard.repository.TransactionRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TransactionServiceImpl implements TransactionService {
+
+    private final TransactionRepository transactionRepository;
+
+    public TransactionServiceImpl(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    @Override
+    public List<Transaction> getAllTransactions() {
+        return transactionRepository.findAll();
+    }
+
+    @Override
+    public Transaction getTransactionById(Long id) {
+        return transactionRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Transaction not found with id: " + id));
+    }
+
+    @Override
+    public Transaction saveTransaction(Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+
+    @Override
+    public void deleteTransaction(Long id) {
+        transactionRepository.deleteById(id);
+    }
+}
